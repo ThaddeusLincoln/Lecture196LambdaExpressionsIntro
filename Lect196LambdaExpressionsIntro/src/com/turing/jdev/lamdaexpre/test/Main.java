e3548928e628e38197db61e7d3a8fdf1e1091aa3
ThaddeusLincoln <beny1419@yahoo.es>
2016-09-18 23:55:01 +0200

    In a LAMBDA-EXPRESSION's body we can also have multiple lines of code, we only need to put the body inside curlu braces, i.e, {...}. Also a semicolon(;) is not necessary when the body of the lamda expression is only one line of coe.

--- Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
+++ Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
@@ -4,7 +4,14 @@ public class Main {
 	
 	public static void main(String[] args){
 	
-		new Thread(()->System.out.println("Mostrando texto desde lamda expression, el cual usa Runnable")).start();
+		new Thread(()->{
+			
+			System.out.println("Mostrando texto desde lamda expression, el cual usa Runnable");
+			System.out.println("Line 2");
+			System.out.println("This is line 3 of the body of the LAMBDA-EXPRESSION");
+			
+			
+		}).start();
 		
 	}
 }
