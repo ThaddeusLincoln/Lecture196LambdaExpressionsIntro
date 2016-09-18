73192ac23eb8e5336a1279c0e1631d745f0faa6f
ThaddeusLincoln <beny1419@yahoo.es>
2016-09-19 00:08:04 +0200

    Sample on how to sort a list of Employee by using Collections.sort, to which a COMPARATOR interface is passed as argument. This COMPARATOR interface might be replaced by a LAMBDA-EXPRESSION later on.

--- Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
+++ Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
@@ -1,5 +1,10 @@
 package com.turing.jdev.lamdaexpre.test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 	
 	public static void main(String[] args){
@@ -12,6 +17,71 @@ public class Main {
 			
 			
 		}).start();
+	
+		// 
+		Employee abraham = new Employee("Abraham Lincoln", 50);
+		Employee rosa = new Employee("Rosa Parks", 35);
+		Employee thaddeuss = new Employee("Thaddeus Stevens", 38);
+		
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(rosa);
+		employees.add(thaddeuss);
+		employees.add(abraham);
+		
+		// let's sort the employees by name in descending order without using Lambda-Expressions
+		// and using the COMPARATOR-Interface
+		
+		Collections.sort(employees, new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee employee1, Employee employee2) {
+				return employee1.getName().compareTo(employee2.getName());
+			}
+			
+		});
+		
+		for(Employee employee : employees){
+			
+			System.out.println(employee.getName());
+		}
+		
+		
+		
+		
+		
+	
+		
+		
 		
 	}
+}
+
+class Employee{
+	
+	private String name;
+	private int age;
+	
+	public Employee(String name, int age){
+		this.setName(name);
+		this.setAge(age);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 }
