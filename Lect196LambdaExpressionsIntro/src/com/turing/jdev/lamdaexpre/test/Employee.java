c631a00ebda43f70696f69b8abf270b7dd6fe54a
ThaddeusLincoln <beny1419@yahoo.es>
2016-09-25 02:33:03 +0200

    Excellent sample of the use of Predicates.

---
+++ Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Employee.java
@@ -0,0 +1,29 @@
+package com.turing.jdev.lamdaexpre.test;
+
+public class Employee{
+	
+	private String name;
+	private int age;
+	
+	public Employee(String name, int age){
+		this.setName(name);
+		this.setAge(age);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
--- Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
+++ Lect196LambdaExpressionsIntro/src/com/turing/jdev/lamdaexpre/test/Main.java
@@ -2,6 +2,7 @@ package com.turing.jdev.lamdaexpre.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 	
@@ -10,110 +11,39 @@ public class Main {
 		Employee abraham = new Employee("Abraham Lincoln", 50);
 		Employee rosa = new Employee("Rosa Parks", 35);
 		Employee thaddeuss = new Employee("Thaddeus Stevens", 38);
+		Employee alexander = new Employee("Alejandro Magno", 32);
+		Employee martinL = new Employee("Martin Luther King", 42);
 		
 		List<Employee> employees = new ArrayList<Employee>();
 		employees.add(rosa);
 		employees.add(thaddeuss);
 		employees.add(abraham);
+		employees.add(alexander);
+		employees.add(martinL);
 		
-		// instead of this
-		/*for(Employee employee : employees){
-			System.out.println(employee.getName());
-			System.out.println(employee.getAge());
-		}*/
-		
-		// we're going to use this lambda-expression functionality
-		employees.forEach(employee -> {
-			System.out.println(employee.getName());
-			System.out.println(employee.getAge());
-			
-		});
-		
+		// 
+		printEmployeesByAge(employees, "Employees over 40", employee -> employee.getAge() > 40);
+		printEmployeesByAge(employees, "Employees under 40", employee -> employee.getAge() <= 40);
 		
 	}
 	
-	// this method is pretty useless, its purpose is to be used as sample when a
-	// lambda expression will have more than one line of code
-	public final static String doStringStuff(UpperConcat uc, String s1, String s2){
-		return uc.upperConcat(s1, s2);
-	}
-}
-
-class Employee{
-	
-	private String name;
-	private int age;
-	
-	public Employee(String name, int age){
-		this.setName(name);
-		this.setAge(age);
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-}
-
-/**
- *Interface with simple method that all it does is upper case two strings
- */
-interface UpperConcat {
-	public String upperConcat(String s1, String s2);
-}
-
-class AnotherClass {
-	
-	public String doSomething(){
+	/**
+	 * Given a predicate it  determines the employees that matches it.
+	 */
+	private static void printEmployeesByAge(List<Employee> employees,
+											String agetText, Predicate<Employee> ageCondition ){
 		
-		// TODO : in the next lecture we'll see how lambda expressions are treated like in nested blocks
-		int i = 0;
-
-		// modifying the arrays value would cause a compile error
-		// i++;
-		
-		// we're going to use a lambda now instead
-		UpperConcat uc = (s1, s2) -> {
-			System.out.println("The lambda expression class's name is: " + getClass().getSimpleName());
-			
-			System.out.println("i in the lambda expression = " + i);
-			
-			String result = s1.toUpperCase() + s2.toUpperCase();
-			return result;
-		};
-		
-		System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
-		return Main.doStringStuff(uc, "String1", "String2");
-		
-	}
-	
-	public void printValue(){
-		
-		int number = 25;
-		Runnable r = () -> {
-			
-			try{
-				Thread.sleep(5000);
-			}catch (InterruptedException e) {
-				e.printStackTrace();
+		System.out.println(agetText);
+		System.out.println("=========================");
+		for(Employee employee : employees){
+			if(ageCondition.test(employee)){
+				System.out.println(employee.getName());
 			}
-			
-			System.out.println("The value is " + number);
-			
-		};
+		}
+		
 		
 	}
 	
 }
+
+
